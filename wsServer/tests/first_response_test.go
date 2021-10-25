82d49244dc40ef6571d622522394f9da2023f56e
agusin <flyingcumbancha@gmail.com>
2021-10-25 17:16:25 -0300

    Added test functions

--- wsServer/tests/first_response_test.go
+++ wsServer/tests/first_response_test.go
@@ -2,38 +2,62 @@ package main
 
 import (
 	"encoding/json"
-	"strings"
+	"fmt"
 	"testing"
 	"websocket"
 )
 
 var primaryUrl = "wss://kovan.infura.io/ws/v3/a0bfa51a18b24e1fac45a36481bf7f61"
 
-func IsSuperAnimal(animal string) bool {
-	return strings.ToLower(animal) == "gopher"
-}
-
-func TestIsSuperAnimal(t *testing.T) {
-	expected := true
-	got := IsSuperAnimal("gopher")
-	if got != expected {
-		t.Errorf("Expected: %v, got: %v", expected, got)
+func printMap(json map[string]interface{}, t *testing.T) {
+	for k, v := range json {
+		t.Log(k, ":", v)
 	}
 }
 
 func TestWeb3ClientVersion(t *testing.T) {
 	messageToSend := "{\"method\":\"web3_clientVersion\",\"params\":[],\"jsonrpc\":\"2.0\",\"id\":67}"
+
+	jsonResponse := WebSocketCall(messageToSend)
+
+	PrintResult(jsonResponse, t)
+}
+
+func TestWeb3Sha3(t *testing.T) {
+	messageToSend := "{\"method\":\"web3_sha3\",\"params\":[\"0x68656c6c6f20776f726c64\"],\"jsonrpc\":\"2.0\",\"id\":67}"
+
+	jsonResponse := WebSocketCall(messageToSend)
+
+	PrintResult(jsonResponse, t)
+}
+
+func TestNetVersion(t *testing.T) {
+
+	messageToSend := "{\"method\":\"net_version\",\"params\":[],\"jsonrpc\":\"2.0\",\"id\":67}"
+
+	jsonResponse := WebSocketCall(messageToSend)
+
+	PrintResult(jsonResponse, t)
+
+}
+
+func WebSocketCall(Message string) []byte {
+
 	primaryDial, _, _ := websocket.DefaultDialer.Dial(primaryUrl, nil)
-	primaryDial.WriteMessage(websocket.TextMessage, []byte(messageToSend))
+	primaryDial.WriteMessage(websocket.TextMessage, []byte(Message))
 	typeMessage, jsonResponse, _ := primaryDial.ReadMessage()
 
 	expectedTypeMessage := websocket.TextMessage
 	if typeMessage != expectedTypeMessage {
-		t.Errorf("Expected: %v, got: %v", expectedTypeMessage, typeMessage)
+		fmt.Printf("Expected: %v, got: %v \n", expectedTypeMessage, typeMessage)
 	}
 
+	return jsonResponse
+}
+
+func PrintResult(Response []byte, t *testing.T) {
 	var result map[string]interface{}
-	json.Unmarshal(jsonResponse, &result)
+	json.Unmarshal(Response, &result)
 	printMap(result, t)
 	searchedAttribute := "result"
 	value, ok := result["result"]
@@ -43,10 +67,5 @@ func TestWeb3ClientVersion(t *testing.T) {
 	if len(value.(string)) == 0 {
 		t.Errorf("Value %v is not a string", value)
 	}
-}
 
-func printMap(json map[string]interface{}, t *testing.T) {
-	for k, v := range json {
-		t.Log(k, ":", v)
-	}
 }
